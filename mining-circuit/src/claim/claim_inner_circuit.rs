f8342bc8e3f8c01bfad3cd7f1f1510152e6e0f34
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-08-30 13:44:47 +0800

    add test_claim_inner_circuit

--- mining-circuit/src/claim/claim_inner_circuit.rs
+++ mining-circuit/src/claim/claim_inner_circuit.rs
@@ -366,3 +366,98 @@ pub fn get_deposit_nullifier_circuit<F: RichField + Extendable<D>, const D: usiz
     let nullifier = Bytes32Target::from_hash_out(builder, input_hash);
     nullifier
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        claim::claim_inner_circuit::{ClaimInnerCircuit, ClaimInnerValue},
+        eligible_tree::{EligibleLeaf, EligibleTree, ELIGIBLE_TREE_HEIGHT},
+    };
+    use intmax2_zkp::{
+        common::{
+            deposit::{get_pubkey_salt_hash, Deposit},
+            salt::Salt,
+            trees::deposit_tree::DepositTree,
+        },
+        constants::DEPOSIT_TREE_HEIGHT,
+        ethereum_types::{
+            address::Address, bytes32::Bytes32, u256::U256, u32limb_trait::U32LimbTrait,
+        },
+    };
+    use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+    use rand::Rng;
+
+    #[test]
+    fn test_claim_inner_circuit() {
+        const D: usize = 2;
+        type C = PoseidonGoldilocksConfig;
+        type F = <C as GenericConfig<D>>::F;
+
+        let mut rng = rand::thread_rng();
+        let n = 10; // number of deposits
+
+        // construct the deposit tree
+        let mut deposit_tree = DepositTree::new(DEPOSIT_TREE_HEIGHT);
+
+        // deposits
+        let mut pubkeys_and_salts = vec![];
+        for _ in 0..n {
+            let pubkey = U256::rand(&mut rng);
+            let salt = Salt::rand(&mut rng);
+            let pubkey_salt_hash = get_pubkey_salt_hash(pubkey, salt);
+            let deposit = Deposit {
+                pubkey_salt_hash,
+                token_index: 0,
+                amount: U256::from(100),
+            };
+            deposit_tree.push(deposit);
+            pubkeys_and_salts.push((pubkey, salt));
+        }
+
+        // construct eligible tree
+        let mut eligible_tree = EligibleTree::new(ELIGIBLE_TREE_HEIGHT);
+        for deposit_index in 0..n {
+            let eligible_leaf = EligibleLeaf {
+                deposit_index,
+                amount: 1,
+            };
+            eligible_tree.push(eligible_leaf);
+        }
+
+        let deposit_tree_root = deposit_tree.get_root();
+        let eligible_tree_root = eligible_tree.get_root();
+
+        // select specified deposit index
+        let deposit_index = rng.gen_range(0..n);
+        let (pubkey, salt) = pubkeys_and_salts[deposit_index as usize];
+
+        let deposit_merkle_proof = deposit_tree.prove(deposit_index as usize);
+        let deposit = deposit_tree.get_leaf(deposit_index as usize);
+
+        let eligible_index = deposit_index; // for now
+        let eligible_merkle_proof = eligible_tree.prove(eligible_index as usize);
+        let eligible_leaf = eligible_tree.get_leaf(eligible_index as usize);
+        assert_eq!(eligible_leaf.deposit_index, deposit_index);
+
+        let recipient = Address::rand(&mut rng);
+        let prev_claim_hash = Bytes32::zero();
+
+        let claim_inner_value = ClaimInnerValue::new(
+            deposit_tree_root,
+            deposit_index,
+            deposit_merkle_proof,
+            deposit,
+            eligible_tree_root,
+            eligible_index,
+            eligible_merkle_proof,
+            eligible_leaf,
+            pubkey,
+            salt,
+            recipient,
+            prev_claim_hash,
+        );
+
+        let claim_inner_circuit = ClaimInnerCircuit::<F, C, D>::new();
+        let _proof = claim_inner_circuit.prove(&claim_inner_value).unwrap();
+    }
+}
