ff4484030158805f8f010ad944b0a99c52fdadbe
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-10 15:50:39 +0800

    parser

---
+++ claim-v0/src/lib.rs
@@ -0,0 +1,20 @@
+pub mod parser;
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+
+    use csv::Reader;
+
+    #[test]
+    fn read_csv() -> anyhow::Result<()> {
+        let file = File::open("data/mainWithdrawals.csv")?;
+        let mut rdr = Reader::from_reader(file);
+        for result in rdr.records() {
+            let record = result?;
+            println!("{:?}", record);
+        }
+
+        Ok(())
+    }
+}
