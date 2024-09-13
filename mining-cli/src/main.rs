2039eb77babf7c6cafe7020b6d5f5b7369678d87
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-13 14:24:52 +0800

    cli: first commit

---
+++ mining-cli/src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    name: String,
+
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
+}
