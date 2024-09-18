a9f99cddf37e5d678d3cdc8c87bd341bcb854e4f
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-19 00:49:04 +0800

    path change

--- cli/src/test/mod.rs
+++ cli/src/test/mod.rs
@@ -16,6 +16,7 @@ pub fn get_dummy_state() -> State {
         "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
     )
     .unwrap();
+
     let mut eligible_tree = EligibleTreeWithMap::new();
     for i in 0..100 {
         eligible_tree.push(EligibleLeaf {
