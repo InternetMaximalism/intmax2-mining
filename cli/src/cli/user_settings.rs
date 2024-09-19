d7edc537bc71ab69b8a1a0b0bfb7606be98dff40
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-19 15:01:33 +0800

    fix parse

--- cli/src/cli/user_settings.rs
+++ cli/src/cli/user_settings.rs
@@ -99,8 +99,8 @@ async fn initial_balance(
     };
 
     let settings = Settings::new()?;
-    let single_deposit_gas_fee: U256 = settings.blockchain.single_deposit_gas_fee.into();
-    let single_claim_gas_fee: U256 = settings.blockchain.sinlge_claim_gas_fee.into();
+    let single_deposit_gas_fee: U256 = settings.blockchain.single_deposit_gas_fee.parse()?;
+    let single_claim_gas_fee: U256 = settings.blockchain.sinlge_claim_gas_fee.parse()?;
     let min_deposit = initial_deposit + single_deposit_gas_fee * num_deposits;
     let min_claim = single_claim_gas_fee * num_deposits;
 
