7e5e97c00a8335aea83988d033180f20f48c2637
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-26 00:18:26 +0800

    deploy script

--- contracts/scripts/int1/local/deploy.ts
+++ contracts/scripts/int1/local/deploy.ts
@@ -1,36 +1,38 @@
 import { ethers, upgrades } from "hardhat";
-import { Int1 } from "../../../typechain-types";
+import { Int1, MinterV1 } from "../../../typechain-types";
 import { cleanEnv, str } from "envalid";
-
-const env = cleanEnv(process.env, {
-  ANALYZER_ADDRESS: str(),
-});
 
 async function main() {
   const [admin] = await ethers.getSigners();
+  const withdrawer = admin;
+  const analyzer = admin;
   console.log(`Deploying contracts with the account: ${admin.address}`);
 
   const withdrawalVerifierFactory = await ethers.getContractFactory(
     "V1WithdrawalPlonkVerifier"
   );
   const withdrawalVerifier = await withdrawalVerifierFactory.deploy();
+  console.log(
+    `Withdrawal verifier deployed at: ${await withdrawalVerifier.getAddress()}`
+  );
 
   const int1Factory = await ethers.getContractFactory("Int1");
   const int1 = (await upgrades.deployProxy(int1Factory, [], {
     initializer: false,
     kind: "uups",
   })) as unknown as Int1;
-
+  await int1.waitForDeployment();
   console.log(`Int1 deployed at: ${await int1.getAddress()}`);
 
   // initialize
   await int1.initialize(await withdrawalVerifier.getAddress(), admin);
-  // grant role
-  await int1.grantRole(await int1.WITHDRAWER(), admin);
+  await int1.grantRole(await int1.WITHDRAWER(), withdrawer);
+  await int1.grantRole(await int1.ANALYZER(), analyzer);
 
   // deploy token
   const tokenFactory = await ethers.getContractFactory("DummyToken");
   const token = await tokenFactory.deploy(admin, ethers.ZeroAddress);
+  await token.waitForDeployment();
   console.log(`Dummy token deployed at: ${await token.getAddress()}`);
 
   const claimVerifierFactory = await ethers.getContractFactory(
@@ -38,14 +40,17 @@ async function main() {
   );
   const claimVerifier = await claimVerifierFactory.deploy();
   const minterFactory = await ethers.getContractFactory("MinterV1");
-  const minter = await minterFactory.deploy(claimVerifier, token, int1, admin);
+  const minter = (await upgrades.deployProxy(minterFactory, [], {
+    initializer: false,
+    kind: "uups",
+  })) as unknown as MinterV1;
+  await minter.waitForDeployment();
   console.log(`Minter deployed at: ${await minter.getAddress()}`);
 
+  // initialize minter
+  await minter.initialize(claimVerifier, token, int1, admin);
   // add token's minter role to minter
   await token.grantRole(await token.MINTER_ROLE(), minter);
-  await minter.mint();
-  const balance = await token.balanceOf(minter);
-  console.log(`Minter's balance: ${ethers.formatEther(balance)}`);
 
   // // fund analyzer
   // const analyzerBalance = await ethers.provider.getBalance(
